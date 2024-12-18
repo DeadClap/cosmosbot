be5267ebe8fe33ec7b213060d59b55570b9ee7e7
Christopher Welch <deadclap893@gmail.com>
2024-12-18 18:50:19 -0500

    Update main file to include dyn load

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,13 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from 'dotenv';
+import { loadEvents } from "./loadEvents";
 
 
 dotenv.config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds]})
+
+loadEvents
 
 try {
     client.login(process.env.DISCORD_TOKEN)
