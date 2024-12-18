2d548530b236978d418014d15c792e4c134abc82
Christopher Welch <deadclap893@gmail.com>
2024-12-18 18:03:52 -0500

    Added Feature: Version in Log

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,14 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from 'dotenv';
+import { version } from '../package.json'
+import getGitBranch from "./utils/gitBranch";
 
 dotenv.config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds]})
 
 client.once('ready', () => {
-    console.log(`Logged in as: ${client.user?.tag}`)
+    console.log(`Logged in as: ${client.user?.tag} (Version: ${version} on branch: ${getGitBranch()})`)
 })
 
 try {
---
+++ src/utils/gitBranch.ts
@@ -0,0 +1,13 @@
+import { execSync } from "child_process";
+
+function getGitBranch(): string {
+    try {
+        const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+        return branch
+    } catch (error) {
+        console.error("Error Fetching git branch: ", error)
+        return 'unknown'
+    }
+}
+
+export default getGitBranch;
