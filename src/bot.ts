7c92620879c5ca984906cb0272e096f2928b85e6
Christopher Welch <deadclap893@gmail.com>
2024-12-18 18:32:31 -0500

    Remove Ready event

--- src/bot.ts
+++ src/bot.ts
@@ -1,15 +1,10 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from 'dotenv';
-import { version } from '../package.json'
-import getGitBranch from "./utils/gitBranch";
+
 
 dotenv.config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds]})
-
-client.once('ready', () => {
-    console.log(`Logged in as: ${client.user?.tag} (Version: ${version} on branch: ${getGitBranch()})`)
-})
 
 try {
     client.login(process.env.DISCORD_TOKEN)
