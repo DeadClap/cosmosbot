182abd327046d9394c3fa7a515605450c1468828
Christopher Welch <deadclap893@gmail.com>
2024-12-18 19:43:12 -0500

    Create Echo

---
+++ src/commands/echo.ts
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "./index";
+
+const EchoCommand: Command = {
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('Replies with your input.')
+        .addStringOption(option => 
+            option.setName('message')
+            .setDescription('The message to echo.')
+            .setRequired(true)
+        ) as SlashCommandBuilder,
+    async execute(interaction) {
+        const message = interaction.options.getString('message', true);
+        await interaction.reply(`You said: ${message}.`)
+    }
+}
+
+export default EchoCommand;
