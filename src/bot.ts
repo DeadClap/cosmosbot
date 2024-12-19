8c99285ebb4db9576497bf04b177b7adf8b76f5a
Christopher Welch <deadclap893@gmail.com>
2024-12-18 19:43:38 -0500

    Implement Loading for Commands and Events

--- src/bot.ts
+++ src/bot.ts
@@ -1,16 +1,21 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from 'dotenv';
 import { loadEvents } from "./loadEvents";
+import { loadCommands } from "./loadCommands";
+import { setCommands } from "./events/commandHandler";
 
 
 dotenv.config()
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds]})
+const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
-loadEvents(client)
-
-try {
+(async () => {
+    console.log('Loading Commands')
+    const commands = await loadCommands();
+    console.log("Setting Commands")
+    setCommands(commands);
+    console.log("Loading events")
+    loadEvents(client);
+    console.log("Logging In")
     client.login(process.env.DISCORD_TOKEN)
-} catch (error) {
-    console.error(error)
-}
+})();
