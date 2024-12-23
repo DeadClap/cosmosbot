6ec58b61be62be9f9d9b9b26588da6c9aef12e12
Christopher Welch <deadclap893@gmail.com>
2024-12-23 15:05:06 -0500

    Move time since to a utility

---
+++ src/utils/timeSince.ts
@@ -0,0 +1,31 @@
+
+
+function timeSince(date: any): string {
+
+    var seconds = Math.floor((new Date() as any - date) / 1000);
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+        return Math.floor(interval) + " year(s) ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " month(s) ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " day(s) ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hour(s) ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minute(s) ago";
+    }
+    return Math.floor(seconds) + " second(s) ago";
+}
+
+export default timeSince;
