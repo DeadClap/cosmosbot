c359ad690c9379ed3b12699cf1099f490590c4b1
Christopher Welch <deadclap893@gmail.com>
2024-12-23 15:05:22 -0500

    Update userinfo to utilize new time since

--- src/commands/user-info.ts
+++ src/commands/user-info.ts
@@ -1,35 +1,7 @@
 import { SlashCommandBuilder, EmbedBuilder, ColorResolvable, Colors } from "discord.js";
 import { Command } from "./index";
-import logger from "../logger";
-import { time } from "console";
+import timeSince from "../utils/timeSince";
 
-function timeSince(date: any) {
-
-    var seconds = Math.floor((new Date() as any - date) / 1000);
-
-    var interval = seconds / 31536000;
-
-    if (interval > 1) {
-        return Math.floor(interval) + " year(s) ago";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-        return Math.floor(interval) + " month(s) ago";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-        return Math.floor(interval) + " day(s) ago";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-        return Math.floor(interval) + " hour(s) ago";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-        return Math.floor(interval) + " minute(s) ago";
-    }
-    return Math.floor(seconds) + " second(s) ago";
-}
 
 const userInfoCommand: Command = {
     data: new SlashCommandBuilder()
