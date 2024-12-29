4faa9b88ea6415890a43138772e9265c311e3612
Christopher Welch <deadclap893@gmail.com>
2024-12-28 19:53:53 -0500

    Added interface for Cat command

---
+++ src/interfaces/Cat.ts
@@ -0,0 +1,9 @@
+// Define the structure of The Cat API response
+export interface CatImage {
+  url: string;
+}
+
+// Define the structure of Meow Facts API response
+export interface CatFactResponse {
+  data: string[];
+}
